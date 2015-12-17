cba2fa7a78e8000a52bc94ed91945cbc5adc7015
Cheng Tan <noinil@gmail.com>
2015-12-17 21:46:53 +0900

    Important: declare evil-leader prefix
    rewrite keybindings of browse-kill-ring and toogle-abbrev-mode

--- layers/ct-editor/keybindings.el
+++ layers/ct-editor/keybindings.el
@@ -1,5 +1,14 @@
 ;;; keybindings.el --- ct-editor Layer keybindings File for Spacemacs
 
+;; --- Important: declare evil-leader prefix ---
+(spacemacs/declare-prefix "o" "user-defined")
+(spacemacs/declare-prefix "ot" "toggle")
+
 (global-set-key (kbd "s-y") 'browse-kill-ring)
 
-(evil-leader/set-key "tA" 'abbrev-mode)
+(spacemacs|add-toggle abbreviation
+  :status abbrev-mode
+  :on (abbrev-mode)
+  :off (abbrev-mode -1)
+  :documentation "Use user defined abbreviations."
+  :evil-leader "ota")
