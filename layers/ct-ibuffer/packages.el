19e5521c9d7ec4aadd26966b24f6bc8dd32f5b47
Cheng Tan <noinil@gmail.com>
2016-01-21 20:05:46 +0900

    more readability added to ibuffer-mode.

--- layers/ct-ibuffer/packages.el
+++ layers/ct-ibuffer/packages.el
@@ -2,9 +2,38 @@
 
 (defun ct-ibuffer/post-init-ibuffer ()
   (with-eval-after-load 'ibuffer
+    (setq ibuffer-show-empty-filter-groups nil)
     (add-hook 'ibuffer-mode-hook 'ibuffer-auto-mode)
+
+    ;; Human readable Size column instead of original one
+    ;; http://www.emacswiki.org/emacs/IbufferMode
+    (define-ibuffer-column size-ct
+      (:name "Size" :inline t)
+      (cond
+       ((> (buffer-size) 1048576) (format "%7.1f M" (/ (buffer-size) 1048576.0)))
+       ((> (buffer-size) 102400) (format "%7.0f K" (/ (buffer-size) 1024.0)))
+       ((> (buffer-size) 1024) (format "%7.1f K" (/ (buffer-size) 1024.0)))
+       (t (format "%8d B" (buffer-size)))))
+
+    (define-ibuffer-column sep-ct
+      (:name " " :inline t) "☛")
+
+    ;; Modify the default ibuffer-formats
+    (setq ibuffer-formats
+          '((mark modified read-only " "
+                  (name 20 20 :left :elide)
+                  " "
+                  (size-ct 10 -1 :right)
+                  " | "
+                  (mode 20 20 :left :elide)
+                  " "
+                  (sep-ct 1 1 :right)
+                  " "
+                  filename-and-process)
+            (mark " " (name 30 -1) " " filename)))
+
+    ;; change update key-binding to "r", "g" is also good.
     (evilified-state-evilify-map ibuffer-mode-map
       :mode ibuffer-mode
       :bindings
-      "g" 'ibuffer-update))
-  (setq ibuffer-show-empty-filter-groups nil))
+      "r" 'ibuffer-update)))
