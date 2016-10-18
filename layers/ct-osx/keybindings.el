f0740e330ba093fd23d68a3a4a05c274632ec4df
Cheng Tan <noinil@gmail.com>
2016-10-18 11:58:46 +0900

    add fix to input backslash in emacx.

--- layers/ct-osx/keybindings.el
+++ layers/ct-osx/keybindings.el
@@ -1,5 +1,6 @@
 ;;; keybindings.el --- ct-osx Layer keybindings File for Spacemacs
 
+(global-set-key (quote [134217893]) "\\")
 (global-set-key (kbd "s-p") 'vkill)
 (evil-leader/set-key "ap" 'vkill)
 (evil-leader/set-key "aP" 'vkill)
