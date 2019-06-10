f7081fd6da4a73293213b4a0b3438130d74f1301
Cheng Tan <noinil@gmail.com>
2019-06-10 12:40:09 +0900

    add fortran layer

---
+++ layers/ct-fortran/packages.el
@@ -0,0 +1,11 @@
+;;; packages.el --- ct-fortran Layer packages File for Spacemacs
+;;
+;; Author: C. Tan
+;; Email:  i@c-tan.com
+;;
+
+(defconst ct-fortran-packages
+  '(
+    ;;what else?
+    ))
+
