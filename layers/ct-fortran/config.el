afe90718ee7d172d9baa3629b7fd01133c1ad98e
Cheng Tan <noinil@gmail.com>
2019-06-10 12:41:11 +0900

    add fortran layer from branch megaminx

---
+++ layers/ct-fortran/config.el
@@ -0,0 +1,21 @@
+;;; config.el --- ct-fortran Layer config File for Spacemacs
+;;
+;; Author: C. Tan
+;; Email:  i@c-tan.com
+;;
+
+;; Fortran settings
+(setq fortran-continuation-string "&")
+(setq fortran-do-indent 2)
+(setq fortran-if-indent 2)
+(setq fortran-structure-indent 2)
+
+;; Fortran 90 settings
+(setq f90-do-indent 2)
+(setq f90-if-indent 2)
+(setq f90-type-indent 2)
+(setq f90-program-indent 2)
+(setq f90-continuation-indent 6)
+(setq f90-smart-end 'blink)
+
+(add-hook 'f90-mode-hook '(lambda () (setq fill-column 80)))
