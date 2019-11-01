57519bb97f9fad414f91f75e105482708f7b3bd3
Cheng Tan <noinil@gmail.com>
2019-11-01 23:51:36 +0900

    add lsp mode and setup lsp-julia.

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
      better-defaults
      helm
      semantic
+     lsp
      git
      github
      org
@@ -51,6 +52,10 @@
      python
      ;; ipython-notebook
      (julia :variables
+            lsp-julia-command "julia"
+            lsp-julia-default-environment "~/.julia/environments/v1.1"
+            julia-mode-enable-lsp t
+            julia-mode-enable-ess nil
             julia-max-block-lookback 5000000)
      ruby
      perl5
